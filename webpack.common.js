2f0e1d2f72f0f990e349c45aa47794129ea3e7bd
Parkhomchuk Yaroslav <parkhomchuk.yaroslav@gmail.com>
2018-01-22 19:16:46 +0200

    html-critical conf new

--- webpack.common.js
+++ webpack.common.js
@@ -5,59 +5,62 @@ const ExtractSCSS = require('extract-text-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-    entry: './src/js/index.js',
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /(node_modules)/,
-                use: {
-                    loader: 'babel-loader',
-                }
+  entry: './src/js/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.(png|jpeg|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              outputPath: 'img/',
+              name: '[name].[ext]',
             },
-            {
-                test: /\.(png|jpeg|jpg|gif)$/,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            name: '[name].[ext]',
-                            outputPath: 'img/',
-                        }
-                    }
-                ]
+          },
+          {
+            loader: 'image-webpack-loader',
+          },
+        ],
+      },
+      {
+        test: /\.(ttf|eot|otf|woff|svg|ico)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/',
             },
-            {
-                test: /\.(ttf|eot|otf|woff|svg)$/,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            name: '[name].[ext]',
-                            outputPath: 'fonts/',
-                        }
-                    }
-                ]
-            }
-        ]
-    },
-    plugins: [
-        new webpack.ProvidePlugin({
-            $: "jquery",
-            jQuery: "jquery",
-            "window.jQuery": "jquery"
-        }),
-        new HtmlWebpackPlugin({
-            template: './src/index.html'
-        }),
-        new ExtractSCSS({
-            filename: 'css/[name].[hash].css',
-        }),
-        new CopyWebpackPlugin([{
-            from: path.join(__dirname,'src','fonts'),
-            to: 'fonts'
-        }])
+          },
+        ],
+      },
     ],
-}
-
-
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery",
+      "window.jQuery": "jquery"
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    }),
+    new ExtractSCSS({
+      filename: 'css/[name].[hash].css',
+    }),
+    new CopyWebpackPlugin([
+      {
+        from: path.join(__dirname,'src','img/favicon'),
+        to: 'img/favicon',
+      },
+    ]),
+  ],
+};
