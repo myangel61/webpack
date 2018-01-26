e571e5ab6a7843163c363198da5e6a2590cccebc
Parkhomchuk Yaroslav <parkhomchuk.yaroslav@gmail.com>
2018-01-26 15:01:00 +0200

    debug error netlify

--- webpack.common.js
+++ webpack.common.js
@@ -29,9 +29,9 @@ module.exports = {
               name: '[name].[ext]',
             },
           },
-          {
-            loader: 'image-webpack-loader',
-          },
+          // {
+          //   loader: 'image-webpack-loader',
+          // },
         ],
       },
       {
