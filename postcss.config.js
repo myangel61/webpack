40345da62a95f13a3f28201bc45121144b87ecbe
Parkhomchuk Yaroslav <parkhomchuk.yaroslav@gmail.com>
2018-03-13 12:14:14 +0200

    initial commit

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-   plugins: [
-      require('autoprefixer'),
-      // require('postcss-sort-alphabetically')
-   ]
+  plugins: [
+    require('autoprefixer'),
+    require('postcss-sort-alphabetically')
+  ],
 };
